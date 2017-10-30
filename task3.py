561504164cbe612709ca77db0c5e4564959b44c2
AnnaSamoylova <33108174+AnnaSamoylova@users.noreply.github.com>
2017-10-30 03:12:31 +0300

    исправлены основные замечания

--- task3.py
+++ task3.py
@@ -1,83 +1,109 @@
-from selenium import webdriver
 import time
 import datetime
-from selenium.common.exceptions import NoSuchElementException
 import unittest
-
-def check_exists_by_xpath(xpath,driver):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
 	
 class testSemrush (unittest.TestCase):
 	driver = None
+	wait = None
+	timeout = 60
+	webdriverPath = "C:\ChromeWebDriver\chromedriver.exe"
+	domain = "https://www.semrush.com"
+	email = "mosik@ya.ru"
+	password = "qwerty123"
 	
+	def get_clickable_element_by_xpath(self, xpath):
+		element = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+		return element
+		
+	def check_exists_by_xpath(self, xpath):
+		try:
+			self.wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+		except TimeoutException:
+			return False
+		return True
 	
 	def setUp(self):
-		self.driver = webdriver.Chrome("C:\ChromeWebDriver\chromedriver.exe")
-		self.driver.get("http://semrush.com")
+		self.driver = webdriver.Chrome(self.webdriverPath)
+		self.wait = WebDriverWait(self.driver, self.timeout)
+		self.driver.get(self.domain)
+		
 		#Залогинивание под пользователем в SEMrush
-		logButton=self.driver.find_element_by_xpath('//button[@data-test="auth-popup__btn-login"]')
+		logButton = self.get_clickable_element_by_xpath('//button[@data-test="auth-popup__btn-login"]')
 		logButton.click()
 		
-		emailBox=self.driver.find_element_by_xpath('//input[@data-test="auth-popup__email"]')
-		emailBox.send_keys("mosik@ya.ru")
+		emailBox = self.get_clickable_element_by_xpath('//input[@data-test="auth-popup__email"]')
+		emailBox.send_keys(self.email)
 		
-		passwordBox=self.driver.find_element_by_xpath('//input[@data-test="auth-popup__password"]')
-		passwordBox.send_keys("qwerty123")
+		passwordBox = self.get_clickable_element_by_xpath('//input[@data-test="auth-popup__password"]')
+		passwordBox.send_keys(self.password)
 		
-		submitButton=self.driver.find_element_by_xpath('//button[@data-test="auth-popup__submit"]')
+		submitButton = self.get_clickable_element_by_xpath('//button[@data-test="auth-popup__submit"]')
 		submitButton.click()
-		time.sleep(20)
 		
 	def tearDown(self):
 		self.driver.quit()
 		
 		
 	def test_login(self):
-		self.assertEqual(self.driver.current_url, 'https://www.semrush.com/dashboard/')
+		self.assertTrue(self.check_exists_by_xpath('//a[@href="/notes/"]'), "Логин провален")
 		
 
 	def test_createNote(self):
-		self.driver.get("http://semrush.com/users/notes")
-		time.sleep(20)
-		addNoteButton=self.driver.find_element_by_xpath('//button[@data-cream-action="add-note"]')
+		notesButton = self.get_clickable_element_by_xpath('//a[@href="/notes/"]')
+		notesButton.click()
+		
+		addNoteButton=self.get_clickable_element_by_xpath('//button[@data-cream-action="add-note"]')
 		addNoteButton.click()
 		
-		titleBox=self.driver.find_element_by_xpath('//input[@data-cream-ui="input-title"]')
+		titleBox=self.get_clickable_element_by_xpath('//input[@data-cream-ui="input-title"]')
+		
+		#Генерируется уникальное имя заметки, т.к. формат выводимой даты 2017-10-30 00:05:04.567499
 		testTitle="Test " + str(datetime.datetime.now())
 		titleBox.send_keys(testTitle)
 		
-		descriptionBox=self.driver.find_element_by_xpath('//textarea[@data-cream-ui="input-note"]')
+		descriptionBox=self.get_clickable_element_by_xpath('//textarea[@data-cream-ui="input-note"]')
 		descriptionBox.send_keys("The Dark Side is more powerfull")
 		
-		saveButton=self.driver.find_element_by_xpath('//button[@data-cream-action="save"]')
+		saveButton=self.get_clickable_element_by_xpath('//button[@data-cream-action="save"]')
 		saveButton.click()
-		time.sleep(20)
 		
-		self.assertTrue(check_exists_by_xpath('//span[contains(text(),"' + testTitle + '")]', self.driver), "Новая заметка не была создана")
+		self.assertTrue(self.check_exists_by_xpath('//span[contains(text(),"' + testTitle + '")]'), "Новая заметка не была создана")
 		
 	def test_newProject(self):
-		self.driver.get("https://www.semrush.com/dashboard/")
-		time.sleep(20)
-		newProjectButton=self.driver.find_element_by_xpath('//i[@class="s-icon -plus -xs"]')
+		newProjectButton=self.get_clickable_element_by_xpath('//i[@class="s-icon -plus -xs"]')
 		newProjectButton.click()
 
-		domainBox=self.driver.find_element_by_xpath('//input[@class="js-pr-watch-domain temp-tn-projects__input"]')
+		domainBox=self.get_clickable_element_by_xpath('//input[@class="js-pr-watch-domain temp-tn-projects__input"]')
 		domainBox.send_keys("ya.ru")
 
-		nameBox=self.driver.find_element_by_xpath('//input[@class="js-pr-watch-name temp-tn-projects__input"]')
+		nameBox=self.get_clickable_element_by_xpath('//input[@class="js-pr-watch-name temp-tn-projects__input"]')
 		nameBox.send_keys("Test")
 
-		createButton=self.driver.find_element_by_xpath('//button[@class="js-pr-create s-btn -xs -success temp-tn-projects__submit"]')
+		createButton=self.get_clickable_element_by_xpath('//button[@class="js-pr-create s-btn -xs -success temp-tn-projects__submit"]')
 		createButton.click()
-		time.sleep(20)
 		
-		self.assertTrue(check_exists_by_xpath('//*[contains(text(),"ya.ru")]', self.driver), "Новый проект не был создан")
+		self.assertTrue(self.check_exists_by_xpath('//*[contains(text(),"ya.ru")]'), "Новый проект не был создан")
+		
+		#удаление проекта
+		settingsButton = self.get_clickable_element_by_xpath('//span[@class="s-icon -s -settings"]')
+		settingsButton.click()
+		
+		deleteButton = self.get_clickable_element_by_xpath('//a[@class="js-remove"]')
+		deleteButton.click()
+		
+		removeBox = self.get_clickable_element_by_xpath('//input[@class="s-input__control js-remove-input"]')
+		removeBox.send_keys("Test")
+		
+		removeButton = self.get_clickable_element_by_xpath('//button[@class="s-btn -s -danger js-remove"]')
+		removeButton.click()
+		
+		#проверить что удалено успешно
+		self.assertTrue(self.check_exists_by_xpath('//span[contains(text(),"Create my first project")]'), "Проект не был удален")
 
 if __name__ == '__main__':
 	unittest.main()
-
-
